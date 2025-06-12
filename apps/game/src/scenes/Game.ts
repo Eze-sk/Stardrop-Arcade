8162929113386f4081c813fccbdaa846090082a1
Eze-sk <ezequielsmiak@gmail.com>
2025-06-12 10:29:36 -0300

    update configuration

--- apps/game/src/scenes/Game.ts
+++ apps/game/src/scenes/Game.ts
@@ -1,19 +1,17 @@
 import Phaser from "phaser"
+import Player from "../objects/Player";
+
+import SCENES from "../const/scenes"
+
 
 export default class Game extends Phaser.Scene {
+  private player!: Player
+
   constructor() {
-    super("game")
-  }
-
-  preload() {
-
+    super({ key: SCENES.GAME })
   }
 
   create() {
-
-  }
-
-  update() {
-
+    this.player = new Player(this, this.cameras.main.width / 2, this.cameras.main.height / 2)
   }
 }
