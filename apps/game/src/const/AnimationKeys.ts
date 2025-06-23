b247b2c94212795ae756e2186784b7862e4ba9ae
Eze-sk <ezequielsmiak@gmail.com>
2025-06-23 17:24:12 -0300

    add an enum with the keys of the animations

---
+++ apps/game/src/const/AnimationKeys.ts
@@ -0,0 +1,10 @@
+enum ANIMATION_KEYS {
+  PLAYER_WALK_DOWN = "playerWalkDown",
+  PLAYER_WALK_LEFT = "playerWalkLeft",
+  PLAYER_WALK_RIGHT = "playerWalkRight",
+  PLAYER_WALK_UP = "playerWalkUp",
+  ENEMY_WALK = "enemyWalk",
+  BACKGROUND_ANIM = "backgroundAnim",
+}
+
+export default ANIMATION_KEYS
