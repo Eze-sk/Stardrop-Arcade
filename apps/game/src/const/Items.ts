4e1b9afa01ed7dbc420bb7bf3bf66e76e67c3673
Eze-sk <ezequielsmiak@gmail.com>
2025-06-23 17:20:52 -0300

    Update probabilities and nomenclature of the "affection" object

--- apps/game/src/const/Items.ts
+++ apps/game/src/const/Items.ts
@@ -3,17 +3,17 @@ import type { ITEMS_TYPE } from "../types/itemsType";
 const ITEMS: ITEMS_TYPE[] = [
   {
     name: "1 Coin",
-    probability: 0.65,
+    probability: 0.3,
     spriteFrame: 0,
-    affection: {
+    effect: {
       upCoins: 1,
     }
   },
   {
     name: "5 Coin",
-    probability: 0.5,
+    probability: 0.2,
     spriteFrame: 1,
-    affection: {
+    effect: {
       upCoins: 5,
     }
   },
@@ -21,51 +21,51 @@ const ITEMS: ITEMS_TYPE[] = [
     name: "1-up",
     probability: 0.4,
     spriteFrame: 8,
-    affection: {
+    effect: {
       upLife: 1
     }
   },
   {
     name: "coffee",
-    probability: 0.5,
+    probability: 0.9,
     spriteFrame: 6,
-    affection: {
+    effect: {
       upSpeed: 2,
       durationEffect: 16 * 1000
     }
   },
   {
     name: "Heavy Machine Gun",
-    probability: 0.2,
+    probability: 0.8,
     spriteFrame: 3,
-    affection: {
+    effect: {
       arms: "HeavyMachine",
       durationEffect: 12 * 1000
     }
   },
   {
     name: "Shotgun",
-    probability: 0.3,
+    probability: 0.7,
     spriteFrame: 7,
-    affection: {
+    effect: {
       arms: "shotgun",
       durationEffect: 12 * 1000
     }
   },
   {
     name: "Sheriff Badge",
-    probability: 0.2,
+    probability: 0.8,
     spriteFrame: 10,
-    affection: {
+    effect: {
       arms: "sheriffBadge",
       durationEffect: 24 * 1000
     }
   },
   {
     name: "Wagon Wheel",
-    probability: 0.1,
+    probability: 0.9,
     spriteFrame: 2,
-    affection: {
+    effect: {
       arms: "sheriffBadge",
       durationEffect: 12 * 1000
     }
