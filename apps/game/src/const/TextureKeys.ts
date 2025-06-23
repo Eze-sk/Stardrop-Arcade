1d7466e343395eac79ee07fa936fe70f978c8787
Eze-sk <ezequielsmiak@gmail.com>
2025-06-23 17:24:33 -0300

    add an enum with the keys of the textures

--- apps/game/src/const/TextureKeys.ts
+++ apps/game/src/const/TextureKeys.ts
@@ -3,6 +3,9 @@ enum TEXTURE_KEYS {
   BULLET = "bullet",
   ITEMS = "items",
   ENEMIES = "enemies",
+  UI = "ui",
+  BACKGROUND = "background",
+  BACKGROUND_ANIM = "background-anim",
 }
 
 export default TEXTURE_KEYS
