09bed0bf0a69dbdfe55e978ea9f9ffc7182ae95a
Eze-sk <ezequielsmiak@gmail.com>
2025-06-28 20:07:00 -0300

    feat: Implement progress bar for level time

--- apps/journey-of-the-prairie-king/src/objects/UI.ts
+++ apps/journey-of-the-prairie-king/src/objects/UI.ts
@@ -4,13 +4,16 @@ import TEXTURE_KEYS from "../const/TextureKeys"
 export default class UI {
   private scene: Phaser.Scene
 
-
   private lifeContainer: Phaser.GameObjects.Container;
   private coinContainer: Phaser.GameObjects.Container;
   private uiContainer: Phaser.GameObjects.Container
   private inventoryContainer: Phaser.GameObjects.Container;
 
   private spriteItemInventory!: Phaser.GameObjects.Sprite;
+
+  private timerBar!: Phaser.GameObjects.Graphics;
+  private timerContainer!: Phaser.GameObjects.Container;
+  private currentBarWidth: number = 534;
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene
@@ -39,15 +42,36 @@ export default class UI {
 
     this.coinContainer = this.scene.add.container(0, 76, [spriteCoin, textCoin])
 
+    // --- TIMER ---
+    const spriteTimer = this.scene.add.sprite(0, 0, TEXTURE_KEYS.UI, 1).setScale(2)
+    this.timerBar = this.scene.add.graphics()
+    this.timerContainer = this.scene.add.container(430, 35, [spriteTimer, this.timerBar])
+
     // --- UI CONTAINER ---
     this.uiContainer = this.scene.add.container(368, 72, [
       this.lifeContainer,
       this.coinContainer,
-      this.inventoryContainer
+      this.inventoryContainer,
     ])
 
     this.uiContainer.setDepth(999)
     this.uiContainer.setScrollFactor(0);
+    this.timerContainer.setDepth(999)
+    this.timerContainer.setScrollFactor(0);
+  }
+
+  updateTimer(progress: number) {
+    this.timerBar.clear()
+    this.timerBar.fillStyle(0x94AE2B, 1)
+
+    const clampedProgress = Phaser.Math.Clamp(progress, 0, 1);
+    const targetWidth = 534 * clampedProgress;
+
+    // Smoothing with linear interpolation
+    this.currentBarWidth = Phaser.Math.Linear(this.currentBarWidth, targetWidth, 0.1);
+
+
+    this.timerBar.fillRect(11, 0, this.currentBarWidth, 9)
   }
 
   updateCoins(coins: number) {
