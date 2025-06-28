3312843c8c48746531b625c5ff932bf6b145aeee
Eze-sk <ezequielsmiak@gmail.com>
2025-06-28 06:58:50 -0300

    Refactor UI class for improved structure and clarity

--- apps/journey-of-the-prairie-king/src/objects/UI.ts
+++ apps/journey-of-the-prairie-king/src/objects/UI.ts
@@ -4,47 +4,68 @@ import TEXTURE_KEYS from "../const/TextureKeys"
 export default class UI {
   private scene: Phaser.Scene
 
-  private spriteInventory!: Phaser.GameObjects.Sprite
-  private spriteItemInventory!: Phaser.GameObjects.Sprite
-  private spriteCoin!: Phaser.GameObjects.Sprite
-  private textCoin!: Phaser.GameObjects.Text
-  private spriteLife!: Phaser.GameObjects.Sprite
-  private textLife!: Phaser.GameObjects.Text
+
+  private lifeContainer: Phaser.GameObjects.Container;
+  private coinContainer: Phaser.GameObjects.Container;
+  private uiContainer: Phaser.GameObjects.Container
+  private inventoryContainer: Phaser.GameObjects.Container;
+
+  private spriteItemInventory!: Phaser.GameObjects.Sprite;
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene
 
-    this.spriteInventory = this.scene.add.sprite(320, 50, TEXTURE_KEYS.UI, 0)
-    this.spriteLife = this.scene.add.sprite(300, 100, TEXTURE_KEYS.UI, 2)
-    this.spriteCoin = this.scene.add.sprite(300, 140, TEXTURE_KEYS.UI, 3)
+    // --- INVENTORY ---
+    const inventorySprite = this.scene.add.sprite(0, 0, TEXTURE_KEYS.UI, 0).setScale(2.2)
+    this.inventoryContainer = this.scene.add.container(20, 0, [inventorySprite])
 
-    this.spriteInventory.setScale(2)
-    this.spriteCoin.setScale(2)
-    this.spriteLife.setScale(2)
-
-    this.textCoin = this.scene.add.text(320, 133, "", {
+    // --- LIFE ---
+    const spriteLife = this.scene.add.sprite(0, 0, TEXTURE_KEYS.UI, 2).setScale(2)
+    const textLife = this.scene.add.text(20, -7, "", {
+      fontFamily: "Pixelify Sans",
       fontSize: '17px',
-      color: '#ffffff'
+      color: '#ffffff',
     })
 
-    this.textLife = this.scene.add.text(320, 93, "", {
+    this.lifeContainer = this.scene.add.container(0, 45, [spriteLife, textLife])
+
+    // --- COINS ---
+    const spriteCoin = this.scene.add.sprite(0, 0, TEXTURE_KEYS.UI, 3).setScale(2)
+    const textCoin = this.scene.add.text(20, -7, "", {
+      fontFamily: "Pixelify Sans",
       fontSize: '17px',
-      color: '#ffffff'
+      color: '#ffffff',
     })
+
+    this.coinContainer = this.scene.add.container(0, 76, [spriteCoin, textCoin])
+
+    // --- UI CONTAINER ---
+    this.uiContainer = this.scene.add.container(368, 72, [
+      this.lifeContainer,
+      this.coinContainer,
+      this.inventoryContainer
+    ])
+
+    this.uiContainer.setDepth(999)
+    this.uiContainer.setScrollFactor(0);
   }
 
   updateCoins(coins: number) {
-    this.textCoin.setText(`x${coins.toString()}`)
+    const text = this.coinContainer.getAt(1) as Phaser.GameObjects.Text
+    text.setText(`x${coins.toString()}`)
   }
 
   updateLife(life: number) {
-    this.textLife.setText(`x${life.toString()}`)
+    const text = this.lifeContainer.getAt(1) as Phaser.GameObjects.Text
+    text.setText(`x${life.toString()}`)
   }
 
   updateInventory(itemsFrame: number) {
+    const inventorySprite = this.inventoryContainer.getAt(0) as Phaser.GameObjects.Sprite
+
     this.spriteItemInventory = this.scene.add.sprite(
-      this.spriteInventory.x,
-      this.spriteInventory.y,
+      inventorySprite.x,
+      inventorySprite.y,
       TEXTURE_KEYS.ITEMS,
       itemsFrame
     )
