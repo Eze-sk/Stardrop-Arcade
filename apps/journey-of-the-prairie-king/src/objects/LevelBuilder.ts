819c579359c559acf05140be3496ace3c89580e8
Eze-sk <ezequielsmiak@gmail.com>
2025-07-09 14:54:44 -0300

    feat: Implement LevelBuilder class for game object management

--- apps/journey-of-the-prairie-king/src/objects/LevelBuilder.ts
+++ apps/journey-of-the-prairie-king/src/objects/LevelBuilder.ts
@@ -1,12 +1,9 @@
 import Phaser from "phaser"
 import TEXTURE_KEYS from "../const/TextureKeys"
 
-// two sprite for background, one for animation and one for static
-
-export default class Background extends Phaser.GameObjects.Sprite {
+export default class LevelBuilder extends Phaser.GameObjects.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, TEXTURE_KEYS.BACKGROUND)
     scene.add.existing(this)
-
   }
 }
